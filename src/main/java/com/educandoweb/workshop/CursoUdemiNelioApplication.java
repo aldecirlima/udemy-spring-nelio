3dba992596086e2a3b9aba0401092c8994b7fec4
Aldecir Lima <aldecirlima@gmail.com>
2021-12-06 21:27:54 -0300

    Project created

---
+++ src/main/java/com/educandoweb/workshop/CursoUdemiNelioApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemiNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemiNelioApplication.class, args);
+	}
+
+}
